d6545321c0fcd37ad1c732de89202d270d13c4c5
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-02-23 17:06:41 +0000

    tidy up

--- run.py
+++ run.py
@@ -94,14 +94,13 @@ def get_last_5_entries_sales():
     and return the data as a list of lists.
     """
     sales = SHEET.worksheet("sales")
-    # column = sales.col_values(3)
-    # print(column)
 
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
-    pprint(columns)
+    
+    return columns
 
 def main():
     """
@@ -117,4 +116,4 @@ def main():
 print("Welcome to Love Sandwitches Data automation")
 #   main()
 
-get_last_5_entries_sales()
+sales_columns = get_last_5_entries_sales()
